87d066450e04fb2e4d8ded07482603367a69d139
sebastiendelestret <58606253+sebastiendelestret@users.noreply.github.com>
2022-03-17 09:14:56 +0100

    first push pilou

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Quel écran souhaitez vous afficher?");
+        System.out.println("1- Ecran prise de commande");
+        System.out.println("2- Ecran cuisine");
+        System.out.println("3- Ecran bar");
+        System.out.println("4- Ecran Monitoring");
+        Scanner scanner = new Scanner(System.in);
+        int choixEcran = scanner.nextInt();
+        System.out.println("Vous avez choisi l'écran: " + choixEcran);
+    }
+}
